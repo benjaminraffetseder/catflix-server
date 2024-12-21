e799e16cc413ad9f1257adb23c414ea1c57667f1
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2024-12-22 00:57:34 +0100

    feat: Add @nestjs/swagger package for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Catflix API')
+    .setDescription('Catflix API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: '/api-json',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
